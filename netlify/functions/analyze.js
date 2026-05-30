36d5c721b506dc9e4c4109474a04597526f2bec6
aleksey199010 <smileman19901@gmail.com>
2026-05-30 15:58:31 +0500

    Update analyze.js

--- netlify/functions/analyze.js
+++ netlify/functions/analyze.js
@@ -7,6 +7,11 @@ exports.handler = async function(event) {
   try {
     const { imageBase64, categories } = JSON.parse(event.body);
 
+    const cleanImage = imageBase64.replace(
+  /^data:image\/[a-zA-Z]+;base64,/,
+  ''
+);
+
     const response = await fetch(
 `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.GEMINI_API_KEY}`      {
         method: 'POST',
@@ -17,7 +22,7 @@ exports.handler = async function(event) {
               {
                 inline_data: {
                   mime_type: 'image/jpeg',
-                  data: imageBase64
+                  data: cleanImage
                 }
               },
               {
